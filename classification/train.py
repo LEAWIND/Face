77f60a8e84b56aa9ea2a89c1524317bdca18bd59
leawind <49919003+LEAWIND@users.noreply.github.com>
2021-02-05 16:35:15 +0800

    ca

--- classification/train.py
+++ classification/train.py
@@ -1,4 +1,4 @@
-import os, re, math
+import os, re, math, time
 pth = os.getcwd().split('\\')
 if pth[-1].lower() == 'face':
 	os.chdir('classification')
@@ -16,14 +16,15 @@ for f in ftList:
 	f = fi.read().split(',')
 	fi.close()
 	f = list(map(float, f))
-	f = torch.tensor(f).to('cuda')
-	f = torch.cat([f, torch.tensor([1]).to('cuda')])
+	f = torch.tensor(f)
+	f = torch.cat([f, torch.tensor([1])])
 	fts.append(f)
 fts = fts[::2]	# 对每个人取 1 张图片就够了
-train_m = fts[   :200]	# 训练用的男性样本
-train_f = fts[250:450]	# 训练用的女性样本
+train_m = fts[   :200][:-1]	# 训练用的男性样本
+train_f = fts[250:450][:-1]	# 训练用的女性样本
 test_m = fts[200:250]	# 测试用的男性样本
 test_f = fts[450:]	# 测试用的女性样本
+
 
 def isMale(ft:'加了常数项的图像特征', prm:'训练好的513个参数'):
 	ft = prm * ft
@@ -47,14 +48,39 @@ def lossOf(prm):	# 损失函数
 		tp = math.log(tp)
 		ls -= tp
 	return ls
+
+
+# while n>0:
+# 	loss_now = loss(p)
+# 	# if n%10 == 1:
+# 	# 	print(p, 'loss =', loss_now)
+# 	# 求偏导, 将结果保存在 d
+# 	d = p.clone()
+# 	q = p.clone()
+# 	for i in range(len(p)):
+# 		q[i] += step
+# 		d[i] = (loss(q) - loss_now) / step
+# 	# 前进
+# 	p -= step*d
+# 	n -= 1
+
+
+
 def move(step=0.00001):	# 每走完一步都把结果存在文件中
 	file = open('target.txt', mode='r')
 	prm = file.read().strip().split(' ')
-	prm = torch.tensor(list(map(float, prm))).to('cuda')
+	prm = torch.tensor(list(map(float, prm)))
 	file.close()
 
-	# l0 = lossOf(prm)
-	
+	l0 = lossOf(prm)
+	print('loss =', l0)
+	d = prm.clone()
+	for i in range(len(prm)):
+		q = prm.clone()
+		q[i] += step
+		d[i] = (lossOf(q) - l0) / step
+
+	prm -= step*d
 
 	prm = map(float, prm)
 	prm = map(str, prm)
@@ -65,7 +91,7 @@ def move(step=0.00001):	# 每走完一步都把结果存在文件中
 	return 
 
 if __name__ == '__main__':
-	n = 4
+	n = 2
 	while n>0:
-		move(0.00001)
+		move(0.006)
 		n -= 1
